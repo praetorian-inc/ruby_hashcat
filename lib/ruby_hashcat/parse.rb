70330c78defb6a7fe63549b9bac8812dc39fa34a
Coleton Pierson <coleton.pierson@gmail.com>
2014-09-04 11:41:43 -0500

    Added parser for HashCat status-automat output format.

--- lib/ruby_hashcat/parse.rb
+++ lib/ruby_hashcat/parse.rb
@@ -101,6 +101,88 @@ module RubyHashcat
       array
     end
 
+
+    ###########################################################################
+    # @method                         RubyHashcat::Parse.status_automat(file) #
+    # @param file:                    [String] Path to oclHashcat stdout file #
+    # @description        Parses the oclHashcat status-automat stdout format. #
+    # @return           [Array][Hash] Array of Hashes with oclHashcat status. #
+    ###########################################################################
+    def self.status_automat(file)
+      array = []
+      return array unless File.exists?(file)
+      string = []
+      File.open(file).each_line do |x|
+        string << x
+      end
+      string.each do |line|
+        line = line.chomp
+        line = line.strip
+        line = line.gsub('  ', ' ')
+        unless line.include?('STATUS')
+          next
+        end
+        split = line.split(' ')
+        stat = {}
+        i = 0
+        if split[i] == 'STATUS'
+          stat[:status] = split[i+1].chomp.strip
+          i += 2
+        end
+        if split[i] == 'SPEED'
+          i += 1
+          speed = 0
+          si = 0
+          while split[i] != 'CURKU'
+            calc = split[i].to_f
+            duration = split[i+1].to_f
+            if calc == 0.0 and duration == 0.0
+              stat["speed_gpu_#{si+1}".to_sym] = 0
+            else
+              stat["speed_gpu_#{si+1}".to_sym] = (calc/duration).round
+              speed += (calc/duration).round
+            end
+            si += 1
+            i += 2
+          end
+          stat[:total_speed] = speed
+        end
+        if split[i] == 'CURKU'
+          stat[:checkpoint] = split[i+1]
+          i += 2
+        end
+        if split[i] == 'PROGRESS'
+          current_prog = split[i+1].to_f
+          total_prog = split[i+2].to_f
+          stat[:progress] = ((((current_prog + 0.0)/(total_prog + 0.0)) * 10000).round)/10000.0
+          i += 3
+        end
+        if split[i] == 'RECHASH'
+          rec_count = split[i+1].to_f.to_i
+          rec_total = split[i+2].to_f.to_i
+          stat[:rec_hash] = "#{rec_count}/#{rec_total}"
+          i += 3
+        end
+        if split[i] == 'RECSALT'
+          rec_count = split[i+1].to_f.to_i
+          rec_total = split[i+2].to_f.to_i
+          stat[:rec_salt] = "#{rec_count}/#{rec_total}"
+          i += 3
+        end
+        if split[i] == 'TEMP'
+          i += 1
+          si = 0
+          while i < split.count
+            stat["temp_gpu_#{si+1}".to_sym] = split[i].to_i
+            si += 1
+            i += 1
+          end
+        end
+        array << stat
+      end
+      array
+    end
+
     ###########################################################################
     # @method                               RubyHashcat::Parse.pot_file(file) #
     # @param file:              [String] Path to oclHashcat cracked passwords #
--- test/test_status.rb
+++ test/test_status.rb
@@ -3,3 +3,5 @@ require "#{path}/../lib/ruby_hashcat"
 require 'pp'
 
 pp RubyHashcat::Parse.stdout('tmp/stdout.txt')[-1]
+
+pp RubyHashcat::Parse.status_automat('tmp/automat.txt')[-1]
